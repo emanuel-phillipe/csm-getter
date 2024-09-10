d8232a488a4af4866fd8ed8f6691ddb4e2b409be
emanuel-phillipe <emanuelphillipe1706@gmail.com>
2024-09-10 17:33:31 -0300

    Removed HTML file dependency

--- get_website.py
+++ get_website.py
@@ -1,31 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from main import get_data
 
-def get_website():
-  driver = webdriver.Chrome()
+def get_html_website_code():
+  options = webdriver.ChromeOptions()
+
+  options.add_argument('headless')
+  options.add_argument('window-size=1920x1080')
+  options.add_argument("disable-gpu")
+
+  driver = webdriver.Chrome(options=options)
+
   driver.get("https://www.sistemas.pucminas.br/sgcaluno/SilverStream/Pages/pgResp_LoginSSL.html")
+
+  print("Entrando no site")
 
   driver.find_element(By.NAME, "S41_").send_keys("892628")
   driver.find_element(By.NAME, "S43_").send_keys("emanuel")
   driver.find_element(By.NAME, "S45_").click()
 
+  print("Logando na sua conta")
   sleep(1)
 
   driver.find_element(By.LINK_TEXT, "Ensino Médio - 3ª série - COLÉGIO SANTA MARIA MINAS - UNIDADE CORAÇÃO EUCARÍSTICO").click()
 
+  print("Aluno selecionado")
   sleep(1)
 
   driver.find_element(By.LINK_TEXT, "Avaliações").click()
 
+  print("Página da avaliações alcançada")
   sleep(1)
 
-  subjects_table = driver.find_element(By.CSS_SELECTOR, "table[width='100%'][border='0'][cellspacing='0'][cellpadding='2']").get_attribute("innerHTML")
+  subjects_table_html = driver.find_element(By.CSS_SELECTOR, "table[width='100%'][border='0'][cellspacing='0'][cellpadding='2']").get_attribute("innerHTML")
 
-  with open('csm_avaliacoes_page.html', 'w', encoding='utf-8') as html_file:
-      html_file.write(subjects_table)
+  print("Página captada")
+  return subjects_table_html
 
-  get_data()
-
-get_website()
+  #with open('csm_avaliacoes_page.html', 'w', encoding='utf-8') as html_file:
+  #    html_file.write(subjects_table)
--- main.py
+++ main.py
@@ -1,63 +1,69 @@
 from bs4 import BeautifulSoup
 import json
+from get_website import get_html_website_code
 
-def get_data():
-  with open('csm_avaliacoes_page.html', 'r', encoding='utf-8') as file:
-    html_content = file.read()
+def get_data(html_page_code):
+    html_content = html_page_code
 
-  # Usar BeautifulSoup para processar o HTML
-  soup = BeautifulSoup(html_content, 'html.parser')
+    # Usar BeautifulSoup para processar o HTML
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-  # Iniciar lista de matérias
-  subjects = []
+    # Iniciar lista de matérias
+    subjects = []
 
-  # Encontrar todas as seções de matérias
-  tables = soup.find_all('table', width="98%")
+    # Encontrar todas as seções de matérias
+    tables = soup.find_all('table', width="98%")
 
-  for table in tables:
-      subject_data = {}
+    for table in tables:
+        subject_data = {}
 
-      # Extrair o nome da matéria
-      subject_name = table.find_previous('td', class_='topoTabelaResponsavel').text.strip()
-      subject_data['subject_name'] = subject_name
+        # Extrair o nome da matéria
+        subject_name = table.find_previous('td', class_='topoTabelaResponsavel').text.strip()
+        subject_data['subject_name'] = subject_name
 
-      # Iniciar lista de avaliações
-      activities = []
-      
-      # Encontrar todas as tables de avaliações
-      rows = table.find_all('tr', bgcolor=["#E5E5E5", "#CCCCCC"])
-      for row in rows:
-          columns = row.find_all('td')
+        # Iniciar lista de avaliações
+        activities = []
+        
+        # Encontrar todas as tables de avaliações
+        rows = table.find_all('tr', bgcolor=["#E5E5E5", "#CCCCCC"])
+        for row in rows:
+            columns = row.find_all('td')
 
-          if len(columns) == 4:
-              activity_data = {
-                  "activity_name": columns[0].text.strip(),
-                  "max": float(columns[1].text.strip()) if columns[1].text.strip() != '-' else None,
-                  "grade": float(columns[2].text.strip()) if columns[2].text.strip() != '-' else None,
-                  "percentage": float(columns[3].text.strip().replace(',', '.')) if columns[3].text.strip() != '-' else None
-              }
-              activities.append(activity_data)
-      
-      # Adicionar avaliações e somatório na matéria
-      subject_data['activities'] = activities 
+            if len(columns) == 4:
+                activity_data = {
+                    "activity_name": columns[0].text.strip(),
+                    "max": float(columns[1].text.strip()) if columns[1].text.strip() != '-' else None,
+                    "grade": float(columns[2].text.strip()) if columns[2].text.strip() != '-' else None,
+                    "percentage": float(columns[3].text.strip().replace(',', '.')) if columns[3].text.strip() != '-' else None
+                }
+                activities.append(activity_data)
+        
+        # Adicionar avaliações e somatório na matéria
+        subject_data['activities'] = activities 
 
-      # Encontrar o somatório
-      somatorio_row = table.find('tr', bgcolor="#CCCCCC")
-      if somatorio_row:
-          somatorio_columns = somatorio_row.find_all('td')
-          subject_data['final_grade'] = {
-              "max": float(somatorio_columns[1].text.strip()),
-              "grade": float(somatorio_columns[2].text.strip()),
-              "percentage": float(somatorio_columns[3].text.strip().replace(',', '.'))
-          }
-      
-      # Adicionar matéria à lista de matérias
-      subjects.append(subject_data)
+        # Encontrar o somatório
+        somatorio_row = table.find('tr', bgcolor="#CCCCCC")
+        if somatorio_row:
+            somatorio_columns = somatorio_row.find_all('td')
+            subject_data['final_grade'] = {
+                "max": float(somatorio_columns[1].text.strip()),
+                "grade": float(somatorio_columns[2].text.strip()),
+                "percentage": float(somatorio_columns[3].text.strip().replace(',', '.'))
+            }
+        
+        # Adicionar matéria à lista de matérias
+        subjects.append(subject_data)
 
-  # Converter a lista de matérias em JSON
-  json_data = {"subjects": subjects}
-  json_output = json.dumps(json_data, indent=4, ensure_ascii=False)
+    # Converter a lista de matérias em JSON
+    json_data = {"subjects": subjects}
+    json_output = json.dumps(json_data, indent=4, ensure_ascii=False)
 
-  # Escrever o JSON
-  with open('subjects_data.json', 'w', encoding='utf-8') as json_file:
-      json_file.write(json_output)
+    # Escrever o JSON
+    with open('subjects_data.json', 'w', encoding='utf-8') as json_file:
+        json_file.write(json_output)
+        print("Arquivo JSON gerado")
+
+    return json_output
+
+html = get_html_website_code()
+print(get_data(html))
